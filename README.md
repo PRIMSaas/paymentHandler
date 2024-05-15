2e49b24c17324db435732424b378902f8aef81a7
emansih <danielquah148@yahoo.com>
2024-05-15 16:55:19 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# Payment Handler for PRIMS
+
+This repository contains the backend code to glue Firebase Authentication, Firestore and Stripe services together. You will need 
+
+### Prerequisites
+1. You will need to make sure you have [npm and nodejs](https://nodejs.org/en) installed. 
+
+2. You have signed up for Stripe account. 
+
+3. You have a file named `config.ts` in `functions/src/`
+```
+# Go here to get your key https://dashboard.stripe.com/test/apikeys
+export let STRIPE_API_KEY: string =  "xxxx";
+
+# This signature is taken after you call stripe listen
+# See running / deploying the software section below. 
+# In a production environment, you can get the key after you add your endpoint
+# https://dashboard.stripe.com/test/webhooks
+export let STRIPE_WEBHOOK_SIGNATURE: string = "xxxx"
+
+export let STRIPE_RETURN_URL: string = "http://127.0.0.1:5500/public/dashboard.html"
+```
+
+4. You have [Stripe CLI](https://docs.stripe.com/stripe-cli) installed
+
+5. You have the Blaze plan for Firebase enabled
+
+6. You have blocking function in Authentication enabled
+
+> Authentication -> Settings -> Blocking Functions
+
+### Running / Deploying the software
+
+To build the software, ensure you have npm installed. 
+```
+cd functions
+npm run install
+npm run build
+```
+https://firebase.google.com/docs/functions/get-started?gen=2nd
+
+To deploy the functions to Firebase
+```
+firebase deploy --only functions
+```
+
+
+### Testing 
+
+To test locally
+```
+firebase emulators:start
+
+### WARNING ###
+### MAKE SURE YOU ARE USING A SANDBOX KEY! ###
+stripe login --api-key xxxx
+
+# Change the URL as required
+stripe listen --forward-to http://127.0.0.1:5001/drjim-payment-demo/us-central1/handleWebhookEvents
+```
+
+Use these test cards to mock payment
+https://docs.stripe.com/testing#cards
---
+++ functions/src/index.ts
@@ -0,0 +1,123 @@
+import { getAuth } from 'firebase-admin/auth';
+import * as functions from 'firebase-functions';
+import * as config from './config.js'
+
+import { onRequest } from "firebase-functions/v2/https";
+import Stripe from 'stripe';
+import { beforeUserCreated } from "firebase-functions/v2/identity";
+ 
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+const stripe = new Stripe(config.STRIPE_API_KEY, {
+   appInfo: {
+     name: 'DrJimPaymentDemo'
+   },
+ });
+ 
+export const handleSubscriptionEvents = onRequest(
+   async (req: functions.https.Request, resp: any) => {
+      const relevantEvents = new Set([
+         'customer.subscription.created',
+         'customer.subscription.updated'
+        ]);
+        let event: Stripe.Event;
+        try {
+         event = stripe.webhooks.constructEvent(
+           req.rawBody,
+           req.headers['stripe-signature'] ?? "",
+           config.STRIPE_WEBHOOK_SIGNATURE
+         );
+       } catch (error) {
+         resp.status(401).send('Webhook Error: Invalid Secret');
+         return;
+       }
+       if (relevantEvents.has(event.type)) {
+         const subscription = event.data.object as Stripe.Subscription;
+         const stripeCustomer = await stripe.customers.retrieve(subscription.customer.toString())
+         if(stripeCustomer.deleted != true){
+            const customerEmail = stripeCustomer.email ?? ""
+            const subscriptionId = subscription.id
+            getAuth().getUserByEmail(customerEmail).then((userRecord) => {
+               const firestoreCollection = "users/".concat(userRecord.uid).concat("/subscriptionDetails")
+               admin.firestore().collection(firestoreCollection).doc(subscriptionId).set({
+                  dateEnd: subscription.current_period_end,
+                  seatPurchased: subscription.items.data.at(0)?.quantity
+               })
+            })
+         }
+      }
+      resp.status(200);   
+   }
+);
+
+// Does not work on emulator. https://stackoverflow.com/a/77798455
+// Only way to test this is to run on the real authentication system. 
+export const beforecreated = beforeUserCreated((event) => {
+   const user = event.data;
+   const currentDate = new Date()
+   const futureDate = new Date(currentDate);
+   futureDate.setDate(currentDate.getDate() + 30);
+   const freeTrialEndInEpoch = Math.floor(futureDate.getTime() / 1000)
+   
+   const firestoreCollection = "users/".concat(user.uid).concat("/subscriptionDetails")
+   admin.firestore().collection(firestoreCollection).doc("FREE_TRIAL").set({
+      dateEnd: freeTrialEndInEpoch,
+    });
+   async() => {
+      const stripeCustomer = await stripe.customers.create({
+         name: user.displayName,
+         email: user.email,
+      }); 
+
+      return {
+         customClaims: {
+            'stripeId': stripeCustomer.id
+         }
+      };
+   }
+   return
+ });
+ 
+ export const createPortalLink = functions.https.onCall(async (data, context) => {
+   const uid = context.auth?.uid;
+   if (!uid) {
+     throw new functions.https.HttpsError(
+       'unauthenticated',
+       'The function must be called while authenticated!'
+     );
+   }
+ 
+   try {
+    const userRecord = await getAuth().getUser(uid);
+    var stripeId = userRecord.customClaims?.['stripeId'];
+ 
+     if (!stripeId) {
+      const stripeCustomer = await stripe.customers.create({
+        name: userRecord.displayName,
+        email: userRecord.email
+      }); 
+      stripeId = stripeCustomer.id
+      getAuth().setCustomUserClaims(uid, {
+        'stripeId': stripeCustomer.id
+      })
+    }
+
+    const stripeCustomer = await stripe.customers.retrieve(stripeId);
+ 
+     if (stripeCustomer.deleted !== true) {
+       const params: Stripe.BillingPortal.SessionCreateParams = {
+         customer: stripeCustomer.id,
+         return_url: config.STRIPE_RETURN_URL
+       };
+ 
+       const session = await stripe.billingPortal.sessions.create(params);
+       return session.url;
+     } else {
+      throw new Error('Customer not found on Stripe\'s system');
+     }
+   } catch (error: any) {
+      throw new functions.https.HttpsError('internal', error.message);
+   }
+ });
